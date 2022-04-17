4e897a846cec0f6cbdd980f03418f4c8840dbaf8
owen <owen013288@gmail.com>
2022-04-17 18:28:34 +0800

    CRUD

--- CoreAPIExercise/Controllers/TodoController.cs
+++ CoreAPIExercise/Controllers/TodoController.cs
@@ -1,5 +1,6 @@
 ﻿using CoreAPIExercise.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,32 +18,81 @@ namespace CoreAPIExercise.Controllers
             _context = context;
         }
 
+        /// <summary>
+        /// 查詢
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IEnumerable<TodoList> Get()
         {
             return _context.TodoList.ToList();
         }
 
+        /// <summary>
+        /// 藉由ID查詢
+        /// </summary>
+        /// <param name="id">TodoList的PK</param>
+        /// <returns></returns>
         [HttpGet("{id}")]
-        public TodoList Get(Guid id)
+        public ActionResult<TodoList> Get(Guid id)
         {
-            return _context.TodoList
-                .Where(x => x.TodoId == id).FirstOrDefault();
+            var result = _context.TodoList.Where(x => x.TodoId == id).FirstOrDefault();
+
+            if (result == null)
+                return NotFound("找不到資源");
+
+            return result;
         }
 
+        /// <summary>
+        /// 新增API
+        /// </summary>
+        /// <param name="value">TodoList的值</param>
+        /// <returns></returns>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<TodoList> Post([FromBody] TodoList value)
         {
+            _context.TodoList.Add(value);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(Get), new { id = value.TodoId }, value);
         }
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(Guid id, [FromBody] TodoList value)
         {
+            if (id != value.TodoId)
+                return BadRequest();
+
+            // using Microsoft.EntityFrameworkCore; => EntityState
+            _context.Entry(value).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (!_context.TodoList.Any(e => e.TodoId == id))
+                    return NotFound();
+                else
+                    return StatusCode(500, "存取發生錯誤");
+            }
+
+            // 更新成功不回傳
+            return NoContent();
         }
 
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(Guid id)
         {
+            var result = _context.TodoList.Find(id);
+            if (result == null)
+                return NotFound();
+            _context.TodoList.Remove(result);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
